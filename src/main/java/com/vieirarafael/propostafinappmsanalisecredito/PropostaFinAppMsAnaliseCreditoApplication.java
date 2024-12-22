40493f73dbfea0d2cefaa895830a002f64d213bd
rafaeldesouzavieira <rafaelralfi@gmail.com>
2024-12-22 17:12:21 -0300

    Initialize Spring Boot project with Gradle setup.
    Set up a Spring Boot microservice with Gradle for dependency management. Added necessary configurations, Gradle wrapper, `.gitignore`, and initial classes including the main application and unit test.

---
+++ src/main/java/com/vieirarafael/propostafinappmsanalisecredito/PropostaFinAppMsAnaliseCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.vieirarafael.propostafinappmsanalisecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaFinAppMsAnaliseCreditoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropostaFinAppMsAnaliseCreditoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vieirarafael/propostafinappmsanalisecredito/PropostaFinAppMsAnaliseCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieirarafael.propostafinappmsanalisecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaFinAppMsAnaliseCreditoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
